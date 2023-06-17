326c16d2a2de61a4f47ca3ed64d13eb0178875b7
¨FerPascualF50¨ <pascualefernando@gmail.com>
2023-06-17 00:21:04 -0300

    feat(agregando componentes comprtidos: boton redenrizado condicional para visalizar cantidad y precio del carrito de compras en el header con su servicio correspondiente)

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './services/products.service';
+import { tap } from 'rxjs/operators';
+import { Product } from './interface/product.interface';
+import { ShoppingCartServise } from 'src/app/shared/services/shopping-cart.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+  products!: Product[];
+
+  constructor (private productSvc: ProductsService, private shoppingCartSvc: ShoppingCartServise) {}
+
+  ngOnInit(): void {
+    this.productSvc.getProducts()
+    .pipe(
+      tap((products: Product[]) => this.products = products)
+    )
+    .subscribe();
+  }
+
+  addToCart(product: Product): void{
+    console.log('Add to Cart', product)
+    this.shoppingCartSvc.updateCart(product);
+  }
+
+}
